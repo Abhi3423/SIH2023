ebb10485d92bd5d0435894afc48a8ff74022f2ec
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-09-13 02:10:03 +0530

    landing page completed

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,12 +1,13 @@
 import download from "../../assets/download.svg"
+import logo from "../../assets/logo.svg"
 
 const Navbar = () => {
     return (
-        <div className="w-[100%] sticky h-18 p-4 shadow-lg shadow-gray-600 justify-between flex text-white">
-            Logo
+        <div className="w-[100%] sticky h-18 py-4 px-8 bg-gradient-to-r from-[#101908] via-transparent to-[#262626] justify-between flex text-white border-b-[0.3px] border-[#c6c6c6]">
+            <img className="w-40" src={logo} alt=""></img>
             <div className="flex gap-8 justify-between items-center text-white text-lg">
-                <button className="hover:text-[#9747FF]">Login/Signup</button>
-                <button className="hover:text-[#299C00] flex gap-2 items-center">Download<img className="w-4" src={download} alt=""></img></button>
+                <button className="hover:text-[#9747FF] hover:font-bold">Login/Signup</button>
+                <button className="hover:text-[#299C00] hover:font-bold flex gap-2 items-center">Download<img className="w-4" src={download} alt=""></img></button>
             </div>
         </div>
     );
