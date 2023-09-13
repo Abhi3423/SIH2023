39fe936ac8274eb58efc51a026fffc0c825eb98d
Abhishek Yadav <abhishekaug2002@gmail.com>
2023-09-14 03:23:56 +0530

    google authentication added

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,47 +1,156 @@
-import axios from "axios"
-import { useState } from "react";
+import Navbar from "../header/header";
+import { Fragment, useEffect, useState } from "react";
+import circles from '../../assets/circles.svg'
+import normal from '../../assets/normal.svg'
+import ddos from '../../assets/ddos.svg'
+import { getCookies, getCookie, setCookie, deleteCookie } from 'cookies-next';
+
 
 const Dashboard = () => {
 
-    const [file, setFile] = useState(null); // State to store the selected file
+    let data = {}
+    data.top = []
+    data.left = []
 
-    const handleFileChange = (event) => {
-        const selectedFile = event.target.files[0];
-        setFile(selectedFile);
-    };
+    const [finaldata, setfinaldata] = useState([])
 
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const formData = new FormData();
-
-        if (file) {
-            formData.append('file', file); // Append the file to the FormData object
+    useEffect(() => {
+        data = JSON.parse(sessionStorage.getItem('network_data'))
+        let keylist = []
+        let toparray = []
+        let leftarray = []
+        for (let key in data) {
+            keylist.push(key)
         }
+        for (let key in data[keylist[0]]) {
 
-        axios
-            .post('http://127.0.0.1:5000/home', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data', // Important for file uploads
-                },
-            })
-            .then(function (response) {
-                console.log(response); // Assuming the API returns data
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
+            // Generate random numbers between 15 and 85
+            let top = Math.floor(Math.random() * (85 - 15 + 1)) + 15;
+            let left = Math.floor(Math.random() * (85 - 15 + 1)) + 15;
 
+            // Append new properties to the object
+            toparray.push(top)
+            leftarray.push(left)
+        }
+        data.top = toparray;
+        data.left = leftarray;
+        setTimeout(() => {
+            console.log(data)
+        }, 5000);
+        setfinaldata(data)
+    }, [])
 
     return (
-        <div className="bg-white">
-            <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
-                <input type="file" onChange={handleFileChange} />
-                <button className="bg-green-400 w-fit rounded-lg p-3" type="submit">Submit</button>
-            </form>
-        </div>
+        <Fragment>
+            <Navbar />
+            <div className="px-8 py-2 flex justify-between bg-white">
+                <span>random_ddos.csv</span>
+                <button className="py-1 px-3 bg-white border-2 border-[#010409] text-[#3A3A3A] font-bold rounded-md">Take Again</button>
+            </div>
+            <div className="flex items-center justify-center py-8 px-20">
+                <div className="flex">
+                    <div className="relative w-[65%]">
+                        <img src={circles} alt="circles" ></img>
+                        <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-white bg-[#114200] rounded-full w-6 h-6"></div>
+                        {/* {Object.keys(data).map((topValue, index) => (
+                            <img
+                                key={index}
+                                className={`absolute top-[${topValue}%] left-[${data.left[index]}%] transform -translate-x-1/2 -translate-y-1/2`}
+                                src={normal}
+                                alt={data.prediction[index]}
+                            />
+                        ))}
+                        {Object.keys(data).map((key, index) => (
+                            <div key={index}>
+                                <h2>{key}</h2>
+                                <ul>
+                                    {data[key].map((value, valueIndex) => (
+                                        <img
+                                            key={index}
+                                            className={`absolute top-[${value}%] left-[${data.left[index]}%] transform -translate-x-1/2 -translate-y-1/2`}
+                                            src={normal}
+                                            alt={data.prediction[index]}
+                                        />
+                                    ))}
+                                </ul>
+                            </div>
+                        ))} */}
+                        {/* {Object.keys(finaldata).map((key, index) => (
+                            <div className="text-white" key={index}>
+                                {console.log(finaldata['left'][index])}
+                                <img
+                                    key={index}
+                                    className={`w- absolute top-[${finaldata['top'][index]}%] left-[${finaldata['left'][index]}%] transform -translate-x-1/2 -translate-y-1/2`}
+                                    src={normal}
+                                 
+                                />
+                            </div>
+                        ))} */}
+                        <img
+                            className={`absolute top-[20%] left-[40%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={normal}
+                        />
+                        <img
+                            className={`absolute top-[30%] left-[80%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={ddos}
+                        />
+                        <img
+                            className={`absolute top-[50%] left-[20%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={ddos}
+                        />
+                        <img
+                            className={`absolute top-[80%] left-[80%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={normal}
+                        />
+                        <img
+                            className={`absolute top-[60%] left-[80%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={normal}
+                        />
+                        <img
+                            className={`absolute top-[90%] left-[20%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={ddos}
+                        />
+                        <img
+                            className={`absolute top-[75%] left-[50%] transform -translate-x-1/2 -translate-y-1/2`}
+                            src={ddos}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col gap-8 w-[60%] items-center text-white">
+                    <div className="rounded-md p-6 bg-[#101908] text-2xl text-center font-bold w-full">NETWORK DETAILS</div>
+                    <div className="flex gap-10 justify-between w-full">
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Source IP</span>
+                            <span className="tracking-wide">10.0.0.1</span>
+                        </div>
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Destination IP</span>
+                            <span className="tracking-wide">10.8.0.1</span>
+                        </div>
+                    </div>
+                    <div className="flex gap-10 justify-between w-full">
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Protocol</span>
+                            <span className="tracking-wide">10</span>
+                        </div>
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Port</span>
+                            <span className="tracking-wide">5762</span>
+                        </div>
+                    </div>
+                    <div className="flex gap-10 justify-between w-full">
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Timestamps</span>
+                            <span className="tracking-wide">13/08/2023 09:52AM</span>
+                        </div>
+                        <div className="flex flex-col rounded-md py-4 w-[80%] bg-[#101908] text-2xl font-medium px-4 justify-between">
+                            <span className="text-xs">Prediction</span>
+                            <span className="tracking-wide">DDoS</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,20 +1,37 @@
 import download from "../../assets/download.svg"
 import logo from "../../assets/logo.svg"
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import { getCookies, getCookie, setCookie, deleteCookie } from 'cookies-next';
 
 const Navbar = () => {
+    const navigate = useNavigate()
+
     return (
         <div className="w-[100%] sticky h-18 py-4 px-8 bg-gradient-to-r from-[#101908] via-transparent to-[#262626] justify-between flex text-white border-b-[0.3px] border-[#c6c6c6]">
-            <img className="w-40" src={logo} alt=""></img>
+            <img onClick={() => navigate('/')} className="w-40" src={logo} alt=""></img>
             <div className="flex gap-8 justify-between items-center text-white text-lg">
-                <button className="transition-colors duration-300 hover:text-[#9747FF]">Login/Signup</button>
-                <button className="transition-colors duration-300 relative group flex gap-2 items-center hover:text-[#299C00]">
-                    Download
-                    <svg
+                <div>
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            console.log(jwt_decode(credentialResponse.credential).sub)
+                            setCookie('token',jwt_decode(credentialResponse.credential).sub)
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />
+                </div>
+
+                <button onClick={() => navigate('/upload')} className="transition-colors duration-300 relative group flex gap-2 items-center hover:text-[#299C00]">
+                    Upload File
+                    {/* <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="22"
                         height="20"
                         viewBox="0 0 22 32"
-                        className="stroke-current group-hover:stroke-current"
+                        className="stroke-current group-hover:stroke-current rotate-180"
                     >
                         <path
                             d="M1.54541 29.7279H20M10.7727 2.0461V23.5764M10.7727 23.5764L18.4621 15.887M10.7727 23.5764L3.08329 15.887"
@@ -22,7 +39,7 @@ const Navbar = () => {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                         />
-                    </svg>
+                    </svg> */}
                 </button>
 
 
